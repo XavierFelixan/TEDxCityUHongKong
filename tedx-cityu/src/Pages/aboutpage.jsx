2f89d33807bdb18027abf45143b820a034fe1e1f
YORIKO <anandaindratan85@gmail.com>
2024-03-26 01:36:40 +0800

    Create aboutpage.jsx

---
+++ tedx-cityu/src/Pages/aboutpage.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import {Banner} from "../Components/banner";
+import { MemberCard } from "../Components/membercard";
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+
+
+
+import Navbar from "../Components/navbar";
+import Footer from "../Components/footer";
