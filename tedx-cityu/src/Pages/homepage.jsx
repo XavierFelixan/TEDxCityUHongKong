df45b103915ffcfc19d11605085015eb6e1dafbe
Xavier Felixan Mijaata <xavier357159@gmail.com>
2024-04-02 14:21:38 +0800

    Fix footer

--- tedx-cityu/src/Pages/homepage.jsx
+++ tedx-cityu/src/Pages/homepage.jsx
@@ -22,7 +22,7 @@ export default function HomePage(){
             <AboutTedx />
             <SpeakerCard/>
             <Performer />
-            <Footer />
+            {/* <Footer /> */}
         </Container>            
     );
 }
