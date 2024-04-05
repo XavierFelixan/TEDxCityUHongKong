4fc22d0ed66b1fe9a84f52e51d66a91a88126e47
Jiiro445 <reynaldoaugustinusluping@gmail.com>
2024-04-05 12:56:52 +0800

    Update PerformerCard.jsx

--- tedx-cityu/src/Components/PerformerCard.jsx
+++ tedx-cityu/src/Components/PerformerCard.jsx
@@ -30,7 +30,7 @@ export default function PerformerCard() {
             {Performerdata.map((item, index) => (
                 <Performer className="my-2 mx-3 pb-5" key={index} to={`/Performer/${item.path}`}>
                     <Card className="flex justify-center items-center overflow-hidden inline-block">
-                        <Image src= {require("../Assets/Members/Performer/" + item.img) } alt={item.fname} className="w-26 h-26 md:w-48 md:h-48 rounded-md transition-transform duration-500 cursor-pointer mx-auto"/>
+                        <Image src= {require("../Assets/Members/Performer/" + item.img) } alt={item.fname} className="w-26 h-26 md:w-48 md:h-48 rounded-md mx-auto"/>
                     </Card>
                     <div className="text-center font-textfont font-bold text-md md:text-3xl mt-2 md:mt-5">
                         {item.fname}
