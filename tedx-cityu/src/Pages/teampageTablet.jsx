7964adb40bf2bdaeb799b176c4552f43e3186a0b
prinprin <prinprin@airpods-plusplusminusmultiply.local>
2025-02-17 23:15:20 +0800

    Tablet version

--- tedx-cityu/src/Pages/teampageTablet.jsx
+++ tedx-cityu/src/Pages/teampageTablet.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import '@splidejs/splide/dist/css/splide.min.css';
 import { styled } from "styled-components";
 import {Banner} from "../Components/banner";
@@ -12,7 +12,6 @@ import financeSponsorshipData from '../Data/financeSponsorshipData.json';
 import humanResourcesData from '../Data/humanResourcesData.json';
 import speakerRelationsData from '../Data/speakerRelationsData.json';
 import marketingCommunicationData from '../Data/marketingCommunicationData.json';
-// import procurementData from '../Data/procurementData.json';
 
 const breakpoints = {
   tablet: '1024px',
@@ -28,123 +27,188 @@ const ContentWrapper = styled.div``;
 const DepartmentList = styled.div``;
 
 const CarouselWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 15px;
 `;
 
-const BackButton = styled.button`
-  display: none;
-  @media (max-width: ${breakpoints.tablet}) {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+const DepartmentButton = styled.button`
     position: absolute;
-    top: 20px;
+    top: 10px;
     left: 20px;
     z-index: 25;
-  }
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: black;
+    padding: 0;
+    background: none;
+    border: none;
+    cursor: pointer;
 `;
 
 const BurgerIcon = styled.div`
-  cursor: pointer;
-  z-index: 20;
-  div {
-    width: 35px;
-    height: 3px;
-    background-color: #333;
-    margin: 10px 0;
-    transition: 0.4s;
-  }
-  @media (min-width: ${breakpoints.tablet}) {
-    display: none;
-  }
+    cursor: pointer;
+    z-index: 20;
+
+    div {
+        width: 35px;
+        height: 3px;
+        background-color: black;
+        margin: 7px 0;
+        transition: 0.4s;
+    }
 `;
 
 const DepartmentWrapper = styled.div`
-  transition: transform 0.3s ease-out;
-  transform: ${({ isOpen }) => isOpen ? 'translateX(0)' : 'translateX(-100%)'};
-  z-index: 1000;
-  @media (max-width: ${breakpoints.tablet}) {
     position: fixed;
     top: 0;
     left: 0;
-    width: 60%;
+    z-index: 1000;
+    width: 40%;
     height: 100%;
     background-color: white;
-    padding: 20px;
+    padding: 0 20px;
     box-shadow: 2px 0px 5px rgba(0, 0, 0, 0.5);
-  }
+    transform: ${({ isOpen }) => (isOpen ? 'translateX(0)' : 'translateX(-100%)')};
+    transition: transform 0.3s ease-out;
+`;
+
+const DepartmentItem = styled.div`
+    cursor: pointer;
+    padding: 10px 0;
+    font-size: 1.3rem;
+    color: gray;
+    border-bottom: 1px solid gray;
+    gap: 15px;
+    transition: color 0.2s, border-color 0.2s;
+
+    &:hover,
+    &:focus {
+        color: black;
+        border-bottom-color: black;
+    }
+
+    ${({ isActive }) =>
+        isActive &&
+        `
+        color: black;
+        border-bottom-color: black;
+    `}
+`;
+
+const CurrentDepartment = styled.div`
+    font-size: 1.75rem;
+    font-weight: bold;
+`;
+
+const MenuHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: sticky;
+    top: 0;
+    z-index: 10;
+    padding: 10px;
+    background-color: white;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
 `;
 
 export default function TeamPage() {
     const [menuOpen, setMenuOpen] = useState(false);
     const [Dept, setDept] = useState('Curators');
-  
+    const departmentWrapperRef = useRef(null);
+
     const list_of_department = {
-      "Curators": curatorData,
-      "Creative": creativeData,
-      "Event Management and Procurement": eventManagementProcurementData,
-      "Finance and Sponsorship": financeSponsorshipData,
-      "Human Resources": humanResourcesData,
-      "Speaker Relations": speakerRelationsData,
-      "Marketing and Communication": marketingCommunicationData,
-      "Technical": technicalData
+        "Curators": curatorData,
+        "Creative": creativeData,
+        "Event Management and Procurement": eventManagementProcurementData,
+        "Finance and Sponsorship": financeSponsorshipData,
+        "Human Resources": humanResourcesData,
+        "Speaker Relations": speakerRelationsData,
+        "Marketing and Communication": marketingCommunicationData,
+        "Technical": technicalData
     };
-  
+
     const handleChange = (department) => {
-      setDept(department);
-    }
-  
+        setDept(department);
+        setMenuOpen(false);
+    };
+
     const RenderDepartment = (Dept) => {
         let data = list_of_department[Dept] || [];
         return (
-          <div>
-            {data.map((item, index) => (
-              <div key={index} className="p-4 mx-auto">
-                <MemberCard
-                  img={require("../Assets/Members/" + item.img)}
-                  fname={item.fname}
-                  lname={item.lname}
-                  major={item.major}
-                  origin={item.origin}
-                  position={item.position}
-                />
-              </div>
-            ))}
-          </div>
-        );
-      };
-  
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-      };
-    
-      return (
-        <Container>
-          <Banner text={"The Crew"} />
-          <BurgerIcon isOpen={!menuOpen} onClick={toggleMenu} className="p-5">
-            <div />
-            <div />
-            <div />
-          </BurgerIcon>
-          <ContentWrapper>
-            <DepartmentWrapper isOpen={menuOpen}>
-              <BackButton onClick={toggleMenu} className="font-bold text-4xl">
-                Back
-              </BackButton>
-              <DepartmentList className="pt-10 text-3xl">
-                {Object.keys(list_of_department).map((department, index) => (
-                    <div key={index} className="cursor-pointer my-8" onClick={() => handleChange(department)}>
-                    {department}
+            <>
+                {data.map((item, index) => (
+                    <div key={index} className="p-4 mx-auto">
+                        <MemberCard
+                            img={require("../Assets/Members/" + item.img)}
+                            fname={item.fname}
+                            lname={item.lname}
+                            major={item.major}
+                            origin={item.origin}
+                            position={item.position}
+                            cardWidth="350px"
+                            cardHeight="550px"
+                        />
                     </div>
                 ))}
-              </DepartmentList>
-            </DepartmentWrapper>
-            <CarouselWrapper>
-              {RenderDepartment(Dept)}
-            </CarouselWrapper>
-          </ContentWrapper>
+            </>
+        );
+    };
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (departmentWrapperRef.current && !departmentWrapperRef.current.contains(event.target)) {
+                setMenuOpen(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [departmentWrapperRef]);
+
+    return (
+        <Container>
+            <Banner text={"The Crew"} />
+            <MenuHeader>
+                <BurgerIcon isOpen={!menuOpen} onClick={toggleMenu} className="p-5">
+                    <div />
+                    <div />
+                    <div />
+                </BurgerIcon>
+                <CurrentDepartment>{Dept}</CurrentDepartment>
+                <div></div>
+            </MenuHeader>
+            <ContentWrapper>
+                <DepartmentWrapper isOpen={menuOpen} ref={departmentWrapperRef}>
+                    <DepartmentButton onClick={toggleMenu}>
+                        Department
+                    </DepartmentButton>
+                    <DepartmentList className="pt-10" style={{ paddingTop: '50px' }}>
+                        {Object.keys(list_of_department).map((department, index) => (
+                            <DepartmentItem
+                                key={index}
+                                onClick={() => handleChange(department)}
+                                isActive={Dept === department}
+                            >
+                                {department}
+                            </DepartmentItem>
+                        ))}
+                    </DepartmentList>
+                </DepartmentWrapper>
+                <CarouselWrapper>
+                    {RenderDepartment(Dept)}
+                </CarouselWrapper>
+            </ContentWrapper>
         </Container>
-      );
-    }
+    );
+}
