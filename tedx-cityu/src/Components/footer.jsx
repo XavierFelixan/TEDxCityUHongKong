22088e52fbb80f9fa34cd36e15871c4a0a50fb01
Hudson Lois <marcushudson228@gmail.com>
2025-02-19 20:34:10 +0800

    Footer Updated

--- tedx-cityu/src/Components/footer.jsx
+++ tedx-cityu/src/Components/footer.jsx
@@ -1,37 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
-import { AiOutlineInstagram, AiOutlineMail } from "react-icons/ai";
+import { AiOutlineInstagram, AiOutlineMail, AiFillLinkedin, AiFillYoutube, AiOutlineFacebook} from "react-icons/ai";
 
 
 const Container = styled.div``
 const FooterContainer = styled.div``
 const CopyrightWrapper = styled.div``;
 const Copyright = styled.p``;
-const SocialMediaWrapper = styled.div``;
+const SocialMediaWrapper = styled.div`
+    // padding-bottom: 1%;
+`;
 const SocialMediaLogoWrapper = styled.a`
     &:hover {
         cursor: pointer;
     }
 `;
+
+const FollowUs = styled.div``;
+
 export default function Footer() {
-    const sizing = "w-7 h-7"
-    const Social_media_logo = [<AiOutlineInstagram className={sizing}/>, /* <AiOutlineMail className={sizing}/> */]
+    const sizing = "w-11 h-11"
+    const Social_media_logo = [<AiOutlineInstagram className={sizing}/>,  <AiFillLinkedin className={sizing}/>,  <AiOutlineFacebook className={sizing}/>, <AiFillYoutube className={sizing}/>]
+    const Link = ["https://www.instagram.com/tedxcityuhongkong/", "https://www.linkedin.com/company/tedxcityuhongkong/", "https://www.facebook.com/TEDxCityUHK/","https://www.youtube.com/@TEDx"]
     return (
         <Container>
-            <FooterContainer className="flex justify-between items-center h-24 w-full bg-black px-5 md:px-12">
-                <CopyrightWrapper className="flex">
-                    <Copyright className="text-gray font-textfont text-md md:text-xl">
-                        Copyright © 2024 <span className="text-white">TEDxCityUHongKong</span>
-                    </Copyright>
-                </CopyrightWrapper>
-                { <SocialMediaWrapper className="flex justify-between items-center font-textfont text-white">
+            <FollowUs className="text-center md:text-7xl text-5xl top-1 relative md:top-2">FOLLOW US!</FollowUs>
+            <FooterContainer className="items-center h-24 w-full bg-black px-5 md:px-12 py-5">
+                { <SocialMediaWrapper className="flex justify-center items-center font-textfont text-white">
                     {Social_media_logo.map((logo, index) => (
-                        <SocialMediaLogoWrapper key={index} target="_blank" href="https://www.instagram.com/tedxcityuhongkong/">
+                        <SocialMediaLogoWrapper key={index} target="_blank" href={Link[index]} className="px-3">
                             {logo}
                         </SocialMediaLogoWrapper>
                     ))}
                 </SocialMediaWrapper> }
+                <CopyrightWrapper className="text-center pt-1">
+                    <Copyright className="text-gray font-textfont text-md md:text-xl">
+                        Copyright© 2025 <span className="text-grey">TEDxCityUHongKong</span>
+                    </Copyright>
+                </CopyrightWrapper>
             </FooterContainer>
         </Container>
     )
