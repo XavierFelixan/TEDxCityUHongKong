fe58b7a1741e6c88b03758f5b924de217e707428
Hudson Lois <marcushudson228@gmail.com>
2025-03-13 19:18:52 +0800

    About page updated

--- tedx-cityu/src/Components/aboutTedx.jsx
+++ tedx-cityu/src/Components/aboutTedx.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 import PastEvent from "../Assets/PastEvent.png";
 import PastSpeaker from "../Assets/Pastspeaker.png";
-import TEDTeam from "../Assets/TEDTeam.png";
+import TEDTeam from "../Assets/TEDxTeam.png";
 
 
 const Container = styled.div``;
@@ -15,7 +15,7 @@ const Tedxdesc = styled.div``;
 
 const Left1 = styled.div`
     float:right;
-    width:30%;
+    width:50%;
     margin: 0 0 0 1.3rem;
 
     @media (max-width: 768px){
@@ -89,31 +89,32 @@ export default function AboutTedx({show = true}){
                         <Container>
                             <Left1>
                                 <ArtWrapper style={{"justify-content":"right"}}>
-                                        <ArtPic src={PastSpeaker} alt="TEDxCityUHongKong 2024 Art"/>
+                                        <ArtPic src={TEDTeam} alt="TEDxCityUHongKong 2024 Art"/>
                                 </ArtWrapper>
                                 
                             </Left1>
                             <Right className="w-full">
-                            <div className="font-bold text-2xl md:text-3xl mb-5">
-                                    About <span className="text-red font-bold">TEDx</span>, <span className="text-red font-bold">x</span> = independently organized event
+                            <div className="font-bold text-2xl md:text-5xl mb-5">
+                                    About <span className="text-red font-bold">TEDx</span><br></br> <span className="text-xl md:text-3xl"><span className="text-red font-bold">x</span> = independently organized event</span>
                                 </div>
-                                <div className="text-base md:text-xl text-justify">
+                                
+                                <hr className="lg:h-[10px] h-[5px] mb-3 " style={{color:'red','background-color':'red'}}/>
+
+                                <div className="text-base md:text-2xl text-justify text-md">
                                     In the spirit of ideas worth spreading, <span className="text-red font-bold">TEDx</span> is a program of local, self-organized events that bring people together to share a <span className="text-red font-bold">TED</span>-like experience. At a <span className="text-red font-bold">TEDx</span> event, <span className="text-red font-bold">TED</span> Talks video and live speakers combine to spark deep discussion and connection. These local, self-organized events are branded <span className="text-red font-bold">TEDx</span>, where <span className="text-red font-bold">x</span> = independently organized <span className="text-red font-bold">TED</span> event. The <span className="text-red font-bold">TED</span> Conference provides general guidance for the <span className="text-red font-bold">TEDx</span> program, but individual <span className="text-red font-bold">TEDx</span> events are self-organized. (Subject to certain rules and regulations.)
                                 </div>
                             </Right>
                         </Container>
                         <Container style={{clear:"both","margin-top":"8%"}}>
-                            <Left2>
-                                <ArtWrapper>
-                                        <ArtPic src={TEDTeam} alt="TEDxCityUHongKong 2024 Art"/>
-                                </ArtWrapper>
-                            </Left2>
 
                             <Right className="w-full">
-                                <div className="font-bold text-2xl md:text-3xl mt-7 mb-5">
+                                <div className="font-bold text-2xl md:text-5xl mb-5">
                                     <span className="font-bold">About </span><span className="text-red font-bold">TED</span>
                                 </div>
-                                <div className="text-base md:text-xl text-justify">
+
+                                <hr className="lg:h-[10px] h-[5px] mb-3 md:w-[48%]" style={{color:'red','background-color':'red'}}/>
+
+                                <div className="text-base md:text-2xl text-justify text-md">
                                     <span className="text-red font-bold">TED</span> is on a mission to discover and spread ideas that spark imagination, embrace possibility and catalyze impact. Our organization is devoted to curiosity, reason, wonder and the pursuit of knowledge — without an agenda. We welcome people from every discipline and culture who seek a deeper understanding of the world and connection with others, and we invite everyone to engage with ideas and activate them in your community.
                                     <span className="text-red font-bold"> TED</span> began in 1984 as a conference where Technology, Entertainment and Design converged, but today it spans a multitude of worldwide communities and initiatives exploring everything from science and business to education, arts and global issues. In addition to the hundreds of <span className="text-red font-bold">TED</span> Talks curated from our annual conferences and published on <span className="text-red font-bold">TED</span>.com, we produce original podcasts, short video series, animated educational lessons (<span className="text-red font-bold">TED</span>-Ed) and TV programs that are translated into more than 100 languages and distributed via partnerships around the world. Each year, more than 3,000 independently run <span className="text-red font-bold">TEDx</span> events bring people together to share ideas and bridge divides in communities on every continent. Through the Audacious Project, <span className="text-red font-bold">TED</span> has helped catalyze more than $3 billion in funding for projects that seek to make the world more beautiful, sustainable and just. In 2020, <span className="text-red font-bold">TED</span> launched Countdown, an initiative to accelerate solutions to the climate crisis and mobilize a movement for a net-zero future. View a full list of <span className="text-red font-bold">TED</span> many programs and initiatives.
                                     <span className="text-red font-bold">TED</span> is owned by a nonprofit, nonpartisan foundation. Our aim is to help create a future worth pursuing for all.
