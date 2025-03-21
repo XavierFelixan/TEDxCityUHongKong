7368711a2806fa6c611c4a1daf4f49d5190f12a0
Hudson Lois <marcushudson228@gmail.com>
2025-03-21 09:41:34 +0800

    Some slide animations implemented

--- tedx-cityu/src/Components/banner.jsx
+++ tedx-cityu/src/Components/banner.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import DarkWave from '../Assets/wave_dark_red.svg';
 import LightWave from '../Assets/wave_tedx_red.svg';
@@ -103,9 +103,50 @@ const TimerBox = styled.div`
     }
 `;
 
+const SlideInContainer = styled.div`
+    opacity: 0;
+    transform: translateY(-100%); 
+    transition: opacity 4s ease-in-out, transform 2s ease-in-out;
+    height:50vh;
+
+    &.slide-in {
+        opacity: 1;
+        transform: translateY(0);
+    }
+`;
+
 const Vector = styled.img.attrs(({ className }) => ({ className }))``;
 
 export const Banner = ({text1,text2, show=true}) =>{
+    const bannerRef = useRef(null); // Ref for BannerWrapper2
+    const [isVisible, setIsVisible] = useState(false); // State to track visibility
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true); // Set visible when in view
+                    observer.unobserve(entry.target); // Stop observing after animation
+                }
+            },
+            {
+                root: null, // Use the viewport as the root
+                rootMargin: '0px',
+                threshold: 0.5, // Trigger when 50% of the element is visible
+            }
+        );
+
+        if (bannerRef.current) {
+            observer.observe(bannerRef.current); // Start observing
+        }
+
+        return () => {
+            if (bannerRef.current) {
+                observer.unobserve(bannerRef.current); // Cleanup observer
+            }
+        };
+    }, []);
+
     return(
         <Container>
             {show && <BannerWrapper1 className="relative flex justify-center items-center min-h-36 md:min-h-[30rem] w-full bg-black">
@@ -118,23 +159,25 @@ export const Banner = ({text1,text2, show=true}) =>{
                 </InnerContainer>
             </BannerWrapper1>}
             
-            {!show && <BannerWrapper2>
-                <ImageContainer>
-                    <img src={Background4} className="w-[80%] mb-10"/>
-                </ImageContainer>
-                <Container>
-                    <TitleContainer>
-                        <Text className="text-black text-4xl lg:text-6xl z-10 pb-3 lg:text-right md:text-right"><span style={{color:'red'}}>TED</span> X <span style={{color:'red'}}>CITYUHONGKONG</span> 2025</Text>
-                        
-                        <hr className="lg:h-[10px] h-[5px]" style={{color:'red','background-color':'red'}}/>
-                        
-                        <Text className="text-black text-5xl lg:text-6xl z-10 lg:pt-12 pt-5 lg:text-right md:text-right">26TH APRIL 2025</Text>
-                        <Text className="text-red text-4xl lg:text-4xl z-10 lg:text-right pb-10 md:text-right">SATURDAY</Text>
-                    </TitleContainer>
-                    <TimerBox className='p-5 flex'>
-                        <Timer/>
-                    </TimerBox>
-                </Container>
+            {!show && <BannerWrapper2 ref={bannerRef}>
+                <SlideInContainer className={isVisible ? 'slide-in' : ''}>
+                    <ImageContainer>
+                        <img src={Background4} className="w-[80%] mb-10"/>
+                    </ImageContainer>
+                    <Container>
+                        <TitleContainer>
+                            <Text className="text-black text-4xl lg:text-6xl z-10 pb-3 lg:text-right md:text-right"><span style={{color:'red'}}>TED</span> X <span style={{color:'red'}}>CITYUHONGKONG</span> 2025</Text>
+                            
+                            <hr className="lg:h-[10px] h-[5px]" style={{color:'red','background-color':'red'}}/>
+                            
+                            <Text className="text-black text-5xl lg:text-6xl z-10 lg:pt-12 pt-5 lg:text-right md:text-right">26TH APRIL 2025</Text>
+                            <Text className="text-red text-4xl lg:text-4xl z-10 lg:text-right pb-10 md:text-right">SATURDAY</Text>
+                        </TitleContainer>
+                        <TimerBox className='p-5 flex'>
+                            <Timer/>
+                        </TimerBox>
+                    </Container>
+                </SlideInContainer>
             </BannerWrapper2>}
             
             
