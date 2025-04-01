559e0e3855a466192a1796654ab6d020f53e3c58
Xavier Felixan Mijaata <xavier357159@gmail.com>
2025-04-01 15:25:20 -0400

    Add redirect for read more

--- tedx-cityu/src/Pages/aboutpage.jsx
+++ tedx-cityu/src/Pages/aboutpage.jsx
@@ -14,7 +14,13 @@ const Container = styled.div``;
 const Description = styled.div``;
 
 
-export default function AboutPage(){
+export default function AboutPage(){   
+     
+    useEffect(() => {
+        // This scrolls the page to the top on component mount
+        window.scrollTo(0, 0);
+    }, []);
+
     return(
         <Container>
             <Banner text1={"ABOUT"} text2 = {<span style={{color:"black"}}>TEDX</span>}/>
